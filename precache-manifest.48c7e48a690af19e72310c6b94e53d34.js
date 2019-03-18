517775ed16014b1ab1b6558f387e2a5360f38445
AdamBremler <adambremler@hotmail.com>
2019-03-18 02:28:13 +0100

    Updates

---
+++ precache-manifest.48c7e48a690af19e72310c6b94e53d34.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "7a62d1d6ec4843655ad9",
+    "url": "/08-weather-app-AdamBremler/static/css/main.9f212587.chunk.css"
+  },
+  {
+    "revision": "7a62d1d6ec4843655ad9",
+    "url": "/08-weather-app-AdamBremler/static/js/main.a17634c9.chunk.js"
+  },
+  {
+    "revision": "84f4a84179044e80a8bd",
+    "url": "/08-weather-app-AdamBremler/static/js/runtime~main.c171354b.js"
+  },
+  {
+    "revision": "fef938c0a2fb8f28fd12",
+    "url": "/08-weather-app-AdamBremler/static/css/2.94080da3.chunk.css"
+  },
+  {
+    "revision": "fef938c0a2fb8f28fd12",
+    "url": "/08-weather-app-AdamBremler/static/js/2.166368fc.chunk.js"
+  },
+  {
+    "revision": "daf15618091e16cf55ba94a0eec61ba9",
+    "url": "/08-weather-app-AdamBremler/static/media/arrow-down.daf15618.svg"
+  },
+  {
+    "revision": "eb77e68573b5fad3d99792582cdf14fb",
+    "url": "/08-weather-app-AdamBremler/static/media/arrow-up.eb77e685.svg"
+  },
+  {
+    "revision": "aa412986a0098dc4a65d09e4a61023ee",
+    "url": "/08-weather-app-AdamBremler/static/media/close.aa412986.svg"
+  },
+  {
+    "revision": "4ee8080e2a4041113e6db4b52fcf88e6",
+    "url": "/08-weather-app-AdamBremler/static/media/humidity.4ee8080e.svg"
+  },
+  {
+    "revision": "77ea8d8d51b15d342668083c507f7d4c",
+    "url": "/08-weather-app-AdamBremler/static/media/wind-speed.77ea8d8d.svg"
+  },
+  {
+    "revision": "8a696c5f46bcc0bbd9631da617da3e90",
+    "url": "/08-weather-app-AdamBremler/static/media/loading.8a696c5f.gif"
+  },
+  {
+    "revision": "000b286c7857a199ad63a047bcf307e1",
+    "url": "/08-weather-app-AdamBremler/static/media/clear-day.000b286c.svg"
+  },
+  {
+    "revision": "f8c59603eaad8423d723ecb458241b72",
+    "url": "/08-weather-app-AdamBremler/static/media/clear-night.f8c59603.svg"
+  },
+  {
+    "revision": "e50dbad25be185da7284df4b23aa5ee3",
+    "url": "/08-weather-app-AdamBremler/static/media/cloudy.e50dbad2.svg"
+  },
+  {
+    "revision": "6302f4083c0f435292982824d957963a",
+    "url": "/08-weather-app-AdamBremler/static/media/fog.6302f408.svg"
+  },
+  {
+    "revision": "be761d07103c3c7ebbd1633ec073f0c5",
+    "url": "/08-weather-app-AdamBremler/static/media/partly-cloudy-day.be761d07.svg"
+  },
+  {
+    "revision": "0e82b0637f3919d5f2d5a077f917fe67",
+    "url": "/08-weather-app-AdamBremler/static/media/partly-cloudy-night.0e82b063.svg"
+  },
+  {
+    "revision": "90687fc32f0eb1375b052f3fba5d7a07",
+    "url": "/08-weather-app-AdamBremler/static/media/rain.90687fc3.svg"
+  },
+  {
+    "revision": "86fed6c0c635636b5afb6233cd199c8b",
+    "url": "/08-weather-app-AdamBremler/static/media/sleet.86fed6c0.svg"
+  },
+  {
+    "revision": "5427c982db6705ca7908863afe5bb82e",
+    "url": "/08-weather-app-AdamBremler/static/media/snow.5427c982.svg"
+  },
+  {
+    "revision": "52175f0bfaf142c1f2d88972716de08a",
+    "url": "/08-weather-app-AdamBremler/static/media/wind.52175f0b.svg"
+  },
+  {
+    "revision": "5cdf1270153061c32e6d3492685cc047",
+    "url": "/08-weather-app-AdamBremler/index.html"
+  }
+];
